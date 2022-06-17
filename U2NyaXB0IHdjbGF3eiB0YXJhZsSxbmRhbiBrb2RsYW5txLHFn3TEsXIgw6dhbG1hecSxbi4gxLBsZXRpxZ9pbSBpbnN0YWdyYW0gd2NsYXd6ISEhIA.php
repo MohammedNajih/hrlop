4ff550613a7522424899c331471106e9a7e517ad
MohammedNajih <102456093+MohammedNajih@users.noreply.github.com>
2022-06-17 08:10:04 +0300

    Add files via upload

---
+++ U2NyaXB0IHdjbGF3eiB0YXJhZsSxbmRhbiBrb2RsYW5txLHFn3TEsXIgw6dhbG1hecSxbi4gxLBsZXRpxZ9pbSBpbnN0YWdyYW0gd2NsYXd6ISEhIA.php
@@ -0,0 +1,30 @@
+
+
+
+
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#000'>
+<body bgcolor='black'>
+<hr size='0.5px' color='white'>
+<font color='yellow'> Kullanıcı Adı: </font><font color='white'>DENEME</font><br>
+<font color='yellow'> Şifre: </font><font color='white'>123321</font><br>
+<font color='yellow'> Şifre2 : </font><font color='white'>123321</font><br>
+<font color='yellow'>Ip Adresi: </font><font color='white'>188.213.34.50</font><br>
+<font color='yellow'>Tarih: </font><font color='white'>08-06-2022 17:53:41</font><br>
+<font color='yellow'>Ülke: </font><font color='white'>Turkey</font><br>
+<font color='yellow'>Şehir: </font><font color='white'>Yukarıdudullu</font><br>
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#000'>
+<body bgcolor='black'>
+
+<font color='green'> Faktör Kodu : </font><font color='white'>23423123</font><br>
+<font color='red'>CODER BY SHuTTeRLooK</font>
+
---
+++ shutterlook.php
@@ -0,0 +1,13 @@
+
+  <html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body bgcolor='#000'>
+<body bgcolor='black'>
+
+<font color='red'> İsim Soyisim: </font><font color='white'>shuterlook</font><br>
+<font color='red'> Mail adrsi: </font><font color='white'>mert@mail.tw</font><br>
+<font color='red'> Telefon numarası: </font><font color='white'>312313</font><br>
+<font color='yellow'>Ip Adresi: </font><font color='white'>188.213.34.50</font><hr size='0.5px' color='white'>
